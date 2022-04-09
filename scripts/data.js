75cf591f5a0583f905d4859292304e5db9c63072
unknown <servrostov@gmail.com>
2022-04-09 20:02:27 +0300

    fix Исправил пометку

--- scripts/data.js
+++ scripts/data.js
@@ -1,33 +1,34 @@
+// Для хранения данных
 // Массив с картинками
 export const initialImages = [{
-    number: 1,
-    alt: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    number: 2,
-    alt: 'Челябинск',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    number: 3,
-    alt: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  }, {
-    number: 4,
-    alt: 'Камчатки',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  }, {
-    number: 5,
-    alt: 'Холмогоры',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  }, {
-    number: 6,
-    alt: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }, {
-    number: 7,
-    alt: 'Ростов-на-Дону',
-    link: 'https://s0.rbk.ru/v6_top_pics/resized/590xH/media/img/3/36/755760558418363.jpg'
-  }
+  number: 1,
+  alt: 'Архыз',
+  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+},
+{
+  number: 2,
+  alt: 'Челябинск',
+  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+},
+{
+  number: 3,
+  alt: 'Иваново',
+  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+}, {
+  number: 4,
+  alt: 'Камчатки',
+  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+}, {
+  number: 5,
+  alt: 'Холмогоры',
+  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+}, {
+  number: 6,
+  alt: 'Байкал',
+  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+}, {
+  number: 7,
+  alt: 'Ростов-на-Дону',
+  link: 'https://s0.rbk.ru/v6_top_pics/resized/590xH/media/img/3/36/755760558418363.jpg'
+}
 ];
